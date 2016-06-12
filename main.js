18945ccbfab9c1bd8ef1840fc2911c2999f2645b
YoshikiIto <yoshikiito.el@gmail.com>
2016-06-12 17:09:58 +0900

    テキストボックスを追加
    アプリケーション起動時にテキストボックスを出すことに成功

---
+++ main.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/*
+// アプリケーションをコントロールするモジュール
+var app = require('app');
+// ウィンドウを作成するモジュール
+var BrowserWindow = require('browser-window');
+
+// メインウィンドウはGCされないようにグローバル宣言
+var mainWindow = null;
+*/
+
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+let mainWindow;
+
+// 全てのウィンドウが閉じたら終了
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
+
+// Electronの初期化完了後に実行
+app.on('ready', function() {
+  // メイン画面の表示。ウィンドウの幅、高さを指定できる
+  mainWindow = new BrowserWindow({width: 1200, height: 600});
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+  
+  // ウィンドウが閉じられたらアプリも終了
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+});
